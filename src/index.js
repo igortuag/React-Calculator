c18c6029f1e8829d1699bbdf862601904deaa52a
Igor Tuag <igortuag@gmail.com>
2020-05-16 13:52:07 -0300

    Componente Calculator

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculator from './main/Calculator';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+        <h1>Calculator</h1>
+        <Calculator />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
